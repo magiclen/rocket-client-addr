0c2a8d1c329232bbb584b44d04209f668143c435
Magic Len <len@magiclen.org>
2018-08-20 20:37:35 +0800

    optimized

--- src/lib.rs
+++ src/lib.rs
@@ -22,54 +22,57 @@ impl<'a, 'r> FromRequest<'a, 'r> for ClientAddr {
     type Error = ();
 
     fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
-        let ip = match request.remote() {
+        let mut from_header = false;
+
+        let remote_ip = match request.remote() {
             Some(addr) => {
                 let ip = addr.ip();
 
                 if is_local_ip(&ip) {
-                    None
-                } else {
-                    Some(ip)
+                    from_header = true;
                 }
+
+                Some(ip)
             }
             None => {
+                from_header = true;
                 None
             }
         };
 
-        match ip {
-            Some(ip) => {
-                Outcome::Success(ClientAddr { ip })
-            }
-            None => {
-                let values: Vec<_> = request.headers().get("x-real-ip").collect();
+        if from_header {
+            let values: Vec<_> = request.headers().get("x-real-ip").collect();
+
+            if values.len() < 1 {
+                let values: Vec<_> = request.headers().get("x-forwarded-for").collect();
 
                 if values.len() < 1 {
-                    let values: Vec<_> = request.headers().get("x-forwarded-for").collect();
-
-                    if values.len() < 1 {
-                        return Outcome::Forward(());
-                    }
-
-                    let value = values[0];
-
-                    return match value.parse::<IpAddr>() {
-                        Ok(ip) => {
-                            Outcome::Success(ClientAddr { ip })
-                        }
-                        Err(_) => Outcome::Forward(())
+                    return match remote_ip {
+                        Some(ip) => Outcome::Success(ClientAddr { ip }),
+                        None => Outcome::Forward(())
                     };
                 }
 
                 let value = values[0];
 
-                match value.parse::<IpAddr>() {
+                return match value.parse::<IpAddr>() {
                     Ok(ip) => {
                         Outcome::Success(ClientAddr { ip })
                     }
                     Err(_) => Outcome::Forward(())
-                }
+                };
             }
+
+            let value = values[0];
+
+            match value.parse::<IpAddr>() {
+                Ok(ip) => {
+                    Outcome::Success(ClientAddr { ip })
+                }
+                Err(_) => Outcome::Forward(())
+            }
+        } else {
+            Outcome::Success(ClientAddr { ip: remote_ip.unwrap() })
         }
     }
 }
